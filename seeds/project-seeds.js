049603dcf78368627c142987598f2f0161261020
Baofeng Guo <peterguo1983@gmail.com>
2022-02-18 15:08:43 -0800

    added the images.

--- seeds/project-seeds.js
+++ seeds/project-seeds.js
@@ -14,7 +14,7 @@ const projectData = [
         "description": "Astronomy Dashboard uses geolocation functions and weather API to present the weather conditions such as visibility and cloudiness, and moon phases.",
         "repo_url": "https://github.com/DazedChou/Astronomy-Dashboard",
         "deployed_url": "https://dazedchou.github.io/Astronomy-Dashboard/",
-        "image_path": "https://github.com/magickw/portfolio/blob/main/assets/images/AstroDash.gif?raw=true"
+        "image_path": "public/images/AstroDash.gif"
       
     },
     {
@@ -22,49 +22,49 @@ const projectData = [
         "description": "CodeQuiz is a timed coding quiz with multiple-choice questions.",
         "repo_url": "https://github.com/magickw/CodeQuiz",
         "deployed_url": "https://magickw.github.io/CodeQuiz/",
-        "image_path": "https://github.com/magickw/portfolio/blob/main/assets/images/codequiz.gif?raw=true"
+        "image_path": "public/images/codequiz.gif"
     },
     {
         "title": "Work Day Scheduler",
         "description": "This simple calendar application that allows a user to save and delete events for each hour of the day.",
         "repo_url": "https://github.com/magickw/CodeQuiz",
         "deployed_url": "https://magickw.github.io/Work-Day-Scheduler/",
-        "image_path": "https://github.com/magickw/portfolio/blob/main/assets/images/scheduler.gif?raw=true"
+        "image_path": "public/images/scheduler.gif"
     },
     {
         "title": "NodeJS README Generator using ES6",
         "description": "This simple calendar application that allows a user to save and delete events for each hour of the day.",
         "repo_url": "https://github.com/magickw/ReadmeGenerator",
         "deployed_url": "",
-        "image_path": "https://github.com/magickw/ReadmeGenerator/raw/main/demo.gif"
+        "image_path": "public/images/README.gif"
     },
     {
         "title": "Note Taker",
         "description": "This application allows you to write notes, save notes and deletes that are being stored in a db.json file.",
         "repo_url": "https://github.com/magickw/NoteTaker",
         "deployed_url": "https://notetaker-guo.herokuapp.com/",
-        "image_path": "https://github.com/magickw/NoteTaker/blob/main/demo/demo.gif?raw=true"
+        "image_path": "public/images/notetaker.gif"
     },
     {
         "title": "Team Profile Generator",
         "description": "This is a command-line application that runs with Node.js to create a simple webpage featuring your development team's roster.",
         "repo_url": "https://github.com/magickw/TeamProfileGenerator",
         "deployed_url": "",
-        "image_path": "https://github.com/magickw/TeamProfileGenerator/raw/main/demo/demo.gif"
+        "image_path": "public/images/teamprofile.gif"
     },
     {
         "title": "E-Commerce Backend",
         "description": "This application creates the backend of an e-commerce website by using Express.js API and Sequelize to interact with a MySQL database.",
         "repo_url": "https://github.com/magickw/E-commerce-backend",
         "deployed_url": "",
-        "image_path": "https://github.com/magickw/E-commerce-backend/raw/main/images/demo.gif"
+        "image_path": "public/images/ecommerce.gif"
     },
     {
         "title": "Employee Tracker",
         "description": "This application is a CLI application using mySQL to create a database management system for employee and company information.",
         "repo_url": "https://github.com/magickw/EmployeeTracker",
         "deployed_url": "",
-        "image_path": "https://github.com/magickw/EmployeeTracker/raw/main/images/demo.gif"
+        "image_path": "public/images/employeetracker.gif"
     },
     {
         "title": "Tech Blog with MVC architectural structure",
