c1bfe9280512a9b7871b59e41681f11d277c89f5
Baofeng Guo <peterguo1983@gmail.com>
2022-02-18 22:03:13 -0800

    added a contact model to take the incoming messages.

---
+++ models/Contact.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Contact extends Model {}
+
+Contact.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: true,
+            }
+        },
+
+        message: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'contact',
+    }
+);
+
+module.exports = Contact;
