ca3f871e05998586e7201bec4d6a7041afad24a5
Baofeng Guo <peterguo1983@gmail.com>
2022-02-21 11:47:34 -0800

    updated the project seeds file.

--- seeds/project-seeds.js
+++ seeds/project-seeds.js
@@ -3,47 +3,47 @@ const { Project } = require('../models/');
 
 const projectData = [
     {
-        "title": "Weather Dashboard",
-        "description": "Full-stack CMS-style blog site where developers can publish and comment, using MVC architectural structure, Handlebars.js, Sequelize, and express-session.",
-        "repo_url": "https://github.com/magickw/WeatherDashboard",
-        "deployed_url": "https://magickw.github.io/WeatherDashboard/",
-        "image_path": "/.images/WeatherDashboard.gif"
+        title: "Weather Dashboard",
+        description: "Full-stack CMS-style blog site where developers can publish and comment, using MVC architectural structure, Handlebars.js, Sequelize, and express-session.",
+        repo_url: "https://github.com/magickw/WeatherDashboard",
+        deployed_url: "https://magickw.github.io/WeatherDashboard/",
+        image_path: "/.images/WeatherDashboard.gif"
     },
     {
-        "title": "AstroDash",
-        "description": "Astronomy Dashboard uses geolocation functions and weather API to present the weather conditions such as visibility and cloudiness, and moon phases.",
-        "repo_url": "https://github.com/DazedChou/Astronomy-Dashboard",
-        "deployed_url": "https://dazedchou.github.io/Astronomy-Dashboard/",
-        "image_path": "./images/AstroDash.gif"
+        title: "AstroDash",
+        description: "Astronomy Dashboard uses geolocation functions and weather API to present the weather conditions such as visibility and cloudiness, and moon phases.",
+        repo_url: "https://github.com/DazedChou/Astronomy-Dashboard",
+        deployed_url: "https://dazedchou.github.io/Astronomy-Dashboard/",
+        image_path: "./images/AstroDash.gif"
       
     },
     {
-        "title": "CodeQuiz",
-        "description": "CodeQuiz is a timed coding quiz with multiple-choice questions.",
-        "repo_url": "https://github.com/magickw/CodeQuiz",
-        "deployed_url": "https://magickw.github.io/CodeQuiz/",
-        "image_path": "./images/codequiz.gif"
+        title: "CodeQuiz",
+        description: "CodeQuiz is a timed coding quiz with multiple-choice questions.",
+        repo_url: "https://github.com/magickw/CodeQuiz",
+        deployed_url: "https://magickw.github.io/CodeQuiz/",
+        image_path: "./images/codequiz.gif"
     },
     {
-        "title": "Work Day Scheduler",
-        "description": "This simple calendar application that allows a user to save and delete events for each hour of the day.",
-        "repo_url": "https://github.com/magickw/CodeQuiz",
-        "deployed_url": "https://magickw.github.io/Work-Day-Scheduler/",
-        "image_path": "./images/scheduler.gif"
+        title: "Work Day Scheduler",
+        description: "This simple calendar application that allows a user to save and delete events for each hour of the day.",
+        repo_url: "https://github.com/magickw/CodeQuiz",
+        deployed_url: "https://magickw.github.io/Work-Day-Scheduler/",
+        image_path: "./images/scheduler.gif"
     },
     {
-        "title": "Note Taker",
-        "description": "This application allows you to write notes, save notes and deletes that are being stored in a db.json file.",
-        "repo_url": "https://github.com/magickw/NoteTaker",
-        "deployed_url": "https://notetaker-guo.herokuapp.com/",
-        "image_path": "./images/notetaker.gif"
+        title: "Note Taker",
+        description: "This application allows you to write notes, save notes and deletes that are being stored in a db.json file.",
+        repo_url: "https://github.com/magickw/NoteTaker",
+        deployed_url: "https://notetaker-guo.herokuapp.com/",
+        image_path: "./images/notetaker.gif"
     },
     {
-        "title": "Tech Blog with MVC architectural structure",
-        "description": "This application is a tech blog which allows the developers to publish blog posts, make comments and create accounts using MVC architectural structure, Handlebars.js, Sequelize.",
-        "repo_url": "https://github.com/magickw/MVCTechBlog",
-        "deployed_url": "https://guotechblog.herokuapp.com/",
-        "image_path": "./images/techblog.gif"
+        title: "Tech Blog with MVC architectural structure",
+        description: "This application is a tech blog which allows the developers to publish blog posts, make comments and create accounts using MVC architectural structure, Handlebars.js, Sequelize.",
+        repo_url: "https://github.com/magickw/MVCTechBlog",
+        deployed_urll: "https://guotechblog.herokuapp.com/",
+        image_path: "./images/techblog.gif"
     },
  
  
